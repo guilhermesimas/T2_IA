47ad07e127d1589f21c2f5965133d76c64f4f86f
guilhermesimas <guilherme_abinader@hotmail.com>
2016-11-15 17:16:13 -0200

    EU QUERO IBAGENS

--- src/MapImage.java
+++ src/MapImage.java
@@ -1,4 +1,8 @@
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 /**
  * Essa classe ai carregar todas as imagens pro Tile e vai guardar elas
@@ -24,20 +28,74 @@ public class MapImage {
 	public static BufferedImage ouro;
 	public static BufferedImage powerup;
 	public static BufferedImage saida;
+	public static BufferedImage nothing;
+	public static BufferedImage duvida;	
 	
-	private static final String norteFile = "norte.png";
-	private static final String sulFile = "sul.png";
-	private static final String lesteFile = "leste.png";
-	private static final String oesteFile = "oeste.png";
-	private static final String buracoFile = "buraco.png";
-	private static final String teleporteFile = "teleporte.png";
-	private static final String inimigoWeakFile = "inimigoWeak.png";
-	private static final String inimigoStrongFile = "inimigoStrong.png";
-	private static final String ouroFile = "ouro.png";
-	private static final String powerupFile = "";
-	private static final String saidaFile = "";
+	private static final String norteFile = "norteFile.png";
+	private static final String sulFile = "sulFile.png";
+	private static final String lesteFile = "lesteFile.png";
+	private static final String oesteFile = "oesteFile.png";
+	private static final String buracoFile = "buracoFile.png";
+	private static final String teleporteFile = "teleportFile.png";
+	private static final String inimigoWeakFile = "inimigoWeakFile.png";
+	private static final String inimigoStrongFile = "inimigoStrongFile.png";
+	private static final String ouroFile = "ouroFile.png";
+	private static final String powerupFile = "powerupFile.png";
+	private static final String saidaFile = "saidaFile.png";
+	private static final String nothingFile = "nothingFile.png";
+	private static final String duvidaFile = "duvidaFile.png";
 	
 	static {
-		
+		try {
+			norte = ImageIO.read(new File(norteFile));
+			sul = ImageIO.read(new File(sulFile));
+			leste = ImageIO.read(new File(lesteFile));
+			oeste = ImageIO.read(new File(oesteFile));
+			buraco = ImageIO.read(new File(buracoFile));
+			teleporte = ImageIO.read(new File(teleporteFile));
+			inimigoWeak = ImageIO.read(new File(inimigoWeakFile));
+			inimigoStrong = ImageIO.read(new File(inimigoStrongFile));
+			ouro = ImageIO.read(new File(ouroFile));
+			powerup = ImageIO.read(new File(powerupFile));
+			saida = ImageIO.read(new File(saidaFile));
+			nothing = ImageIO.read(new File(nothingFile));
+			duvida = ImageIO.read(new File(duvidaFile));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+	
+	public BufferedImage getImage(char c){
+		switch(c){
+		case 'n':
+			return norte;
+		case 's':
+			return sul;
+		case 'e':
+			return leste;
+		case 'w':
+			return oeste;
+		case 'P':
+			return buraco;
+		case 'T':
+			return teleporte;
+		case 'd':
+			return inimigoWeak;
+		case 'D':
+			return inimigoStrong;
+		case 'O':
+			return ouro;
+		case 'U':
+			return powerup;
+		case 'x':
+			return saida;
+		case '.':
+			return nothing;
+		case '?':
+			return duvida;
+		default:
+			return ouro;			
+		}
+	}
+	
 }
